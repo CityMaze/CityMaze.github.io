18f59cb3485ec79af26341a0204678473eea22d5
CityMaze <daniel5602000@gmail.com>
2017-12-08 17:16:50 +0800

    1208
    1208

---
+++ js/main.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        var d = $("#top-floor");
+        d.text(Math.floor($(window).scrollTop() / 10));
+    });
+    // 幫 a.abgne_gotoheader 加上 click 事件
+    $('#P1').click(function () {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 1000
+        }, 600);
+        return false;
+    });
+    $('#P2').click(function () {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 2000
+        }, 600);
+        return false;
+    });
+    $('#P3').click(function () {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 3000
+        }, 600);
+        return false;
+    });
+    $('#P4').click(function () {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 4000
+        }, 600);
+        return false;
+    });
+    $('#P5').click(function () {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 5000
+        }, 600);
+        return false;
+    });
+    $('#P6').click(function () {
+        // 讓捲軸用動畫的方式移動到 0 的位置
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+            scrollTop: 6000
+        }, 600);
+        return false;
+    });
+});
